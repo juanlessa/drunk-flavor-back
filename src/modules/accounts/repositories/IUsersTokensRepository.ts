b5dc5500515e01c72c8a0dd5a0692164caa1c992
Juan Lessa <juanvlessa@ua.pt>
2023-05-30 16:53:22 +0100

    refactor: :recycle: userTokens repository update

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -2,7 +2,7 @@ import { IUserToken } from "@modules/accounts/dtos/UsersTokensDTO";
 
 interface IUsersTokensRepository {
     create({user_id, expires_date, refresh_token }: IUserToken): Promise<IUserToken>;
-    //findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<IUserToken>;
+    findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<IUserToken>;
     //deleteById(id: string): Promise<void>;
     //findByRefreshToken(refresh_token: string): Promise<IUserToken>;
 }
