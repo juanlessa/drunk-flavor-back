ae4fe6e5dad2d342d007e1f101771c8c94018080
Juan Lessa <juanvlessa@ua.pt>
2023-06-10 00:33:53 +0100

    refactor: :recycle: change token expireIn

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -4,6 +4,7 @@ dotenv.config()
 export default {
     secret_token: process.env.TOKEN_SECRET,
     expires_in_token: process.env.TOKEN_EXPIRES_IN,
+    expires_token_hours: Number(process.env.TOKEN_EXPIRES_HOURS),
     secret_refresh_token: process.env.REFRESH_TOKEN_SECRET,
     expires_in_refresh_token: process.env.REFRESH_TOKEN_EXPIRES_IN,
     expires_refresh_token_days: Number(process.env.REFRESH_TOKEN_EXPIRES_DAYS),
