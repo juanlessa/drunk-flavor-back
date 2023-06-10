3dcaceeb1972c3cbbde3b4838ccef8140f2a32d1
Juan Lessa <juanvlessa@ua.pt>
2023-06-10 17:06:13 +0100

    refactor: :recycle: formatting drinks module

--- src/modules/drinks/useCases/updateIngredient/UpdateIngredientController.ts
+++ src/modules/drinks/useCases/updateIngredient/UpdateIngredientController.ts
@@ -1,32 +1,26 @@
-import { Request, Response } from "express";
-import { container } from "tsyringe";
-import { UpdateIngredientService } from "./UpdateIngredientService";
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { UpdateIngredientService } from './UpdateIngredientService';
 
 interface IRequest {
-    id: string;
-    name: string;
-    category: string,
-    isAlcoholic: boolean,
-    unity: string,
-    colorTheme: string
+	id: string;
+	name: string;
+	category: string;
+	isAlcoholic: boolean;
+	unity: string;
+	colorTheme: string;
 }
 
-
 class UpdateIngredientController {
-    async handle(request: Request, response: Response): Promise<Response> {
-        const { id, name, category, isAlcoholic, unity, colorTheme }:IRequest = request.body;
-        
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { id, name, category, isAlcoholic, unity, colorTheme }: IRequest = request.body;
 
-        const updateIngredientService = container.resolve(
-            UpdateIngredientService
-        );
+		const updateIngredientService = container.resolve(UpdateIngredientService);
 
-        await updateIngredientService.execute(
-            { id, name, category, isAlcoholic, unity, colorTheme }
-        );
+		await updateIngredientService.execute({ id, name, category, isAlcoholic, unity, colorTheme });
 
-        return response.status(204).send();
-    }
+		return response.status(204).send();
+	}
 }
 
 export { UpdateIngredientController };
