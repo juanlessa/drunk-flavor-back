265b0a73c0e770d54ab4275a2855fe9f9a7bf1e8
Juan Lessa <juanvlessa@ua.pt>
2023-06-19 10:39:45 +0100

    fix: :bug: Fix delete file bug on AWS s3

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { S3, S3Client } from '@aws-sdk/client-s3';
+import { S3, S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import fs from 'fs';
 
-const s3Config = new S3Client({
+const s3 = new S3Client({
 	region: process.env.AWS_DEFAULT_REGION,
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -23,22 +23,15 @@ export const deleteFile = async (fileName: string): Promise<void> => {
 		await fs.promises.unlink(fileName);
 		return;
 	}
-	const s3 = new S3(s3Config);
 	try {
-		console.log('file name ', fileName);
-
-		await s3.headObject({
+		const command = new DeleteObjectCommand({
 			Bucket: process.env.AWS_S3_BUCKET_NAME,
 			Key: fileName
 		});
+		await s3.send(command);
 	} catch (error) {
-		console.error('find file error');
+		console.error('delete file error');
 		console.error(error);
-
 		return;
 	}
-	await s3.deleteObject({
-		Bucket: process.env.AWS_S3_BUCKET_NAME,
-		Key: fileName
-	});
 };
