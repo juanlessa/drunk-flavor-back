12a9d696f06a1549bad3683190828d4cd0a0dd5a
Juan Lessa <juanvlessa@ua.pt>
2023-06-22 20:53:45 +0100

    feat: :sparkles: is admin middleware

---
+++ src/shared/infra/http/middlewares/isAdminUser.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { UsersRepository } from '@modules/accounts/infra/prisma/repositories/UsersRepository';
+import AppError from '@errors/AppError';
+
+const roles = {
+	ADMIN: 'admin'
+};
+
+export async function isUserAdmin(request: Request, response: Response, next: NextFunction) {
+	const { id: userId } = request.user;
+	if (!userId) {
+		throw new AppError('user is not authenticated', 401);
+	}
+
+	const usersRepository = new UsersRepository();
+
+	const user = await usersRepository.findById(userId);
+	if (!user) {
+		throw new AppError('User does not exist', 401);
+	}
+
+	if (user.role !== roles.ADMIN) {
+		throw new AppError('invalid permissions', 403);
+	}
+	next();
+}
