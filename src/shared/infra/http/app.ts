754a7580bc5574a21db0a7e52bcc7e8940db6ef7
Juan Lessa <juanvlessa@ua.pt>
2023-07-01 17:36:43 +0100

    feat: :sparkles: swagger documentation

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from '@routes/index';
+import swaggerFile from '../../../swagger.json';
+import swaggerUi from 'swagger-ui-express';
 import '@shared/container';
 import AppError from '@shared/errors/AppError';
 
@@ -14,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/files', express.static(resolve(__dirname, '..', 'tmp', 'drink')));
 app.use(routes);
 
