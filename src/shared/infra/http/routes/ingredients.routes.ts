6b73cafdde158b29947284b4fe151c58a742d3d4
Juan Lessa <juanvlessa@ua.pt>
2023-08-02 22:42:02 +0100

    style: :art: formatting

--- src/shared/infra/http/routes/ingredients.routes.ts
+++ src/shared/infra/http/routes/ingredients.routes.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
 import { CreateIngredientController } from '@modules/drinks/useCases/createIngredient/CreateIngredientController';
-import { ListIngredientsController } from '@modules/drinks/useCases/listIngredients/ListIngredientsController';
-import { UpdateIngredientController } from '@modules/drinks/useCases/updateIngredient/UpdateIngredientController';
 import { DeleteIngredientController } from '@modules/drinks/useCases/deleteIngredient/DeleteIngredientController';
 import { GetIngredientController } from '@modules/drinks/useCases/getIngredient/GetIngredientController';
-
+import { ListIngredientsController } from '@modules/drinks/useCases/listIngredients/ListIngredientsController';
+import { UpdateIngredientController } from '@modules/drinks/useCases/updateIngredient/UpdateIngredientController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { Router } from 'express';
+
 const createIngredientController = new CreateIngredientController();
 const listIngredientsController = new ListIngredientsController();
 const updateIngredientController = new UpdateIngredientController();
