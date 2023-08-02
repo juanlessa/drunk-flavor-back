63953e156945f48c60985fc895032eab30cb3afa
Juan Lessa <juanvlessa@ua.pt>
2023-08-02 22:43:16 +0100

    feat: :sparkles: categories repository dependency injection

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authenticateRoutes from './authenticate.routes';
 import drinksRoutes from './drinks.routes';
 import ingredientsRoutes from './ingredients.routes';
+import categoriesRoutes from './categories.routes';
 import usersRoutes from './users.routes';
 
 const router = Router();
 
 router.use('/ingredients', ingredientsRoutes);
+router.use('/categories', categoriesRoutes);
 router.use('/drinks', drinksRoutes);
 router.use('/users', usersRoutes);
 router.use(authenticateRoutes);
