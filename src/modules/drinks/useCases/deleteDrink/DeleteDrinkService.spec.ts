a37871c890c573265f7fed3c983cdf9695c7bd8a
Juan Lessa <juanvlessa@ua.pt>
2023-08-05 01:58:49 +0100

    refactor: :recycle: delete drink using prisma relations

--- src/modules/drinks/useCases/deleteDrink/DeleteDrinkService.spec.ts
+++ src/modules/drinks/useCases/deleteDrink/DeleteDrinkService.spec.ts
@@ -1,13 +1,16 @@
 import AppError from '@errors/AppError';
-import { ICreateIngredient } from '@modules/drinks/dtos/ingredients';
+import Category from '@modules/drinks/entities/Category';
+import Ingredient from '@modules/drinks/entities/Ingredient';
+import { DRINK_ERRORS } from '@modules/drinks/errors/drinkErrors';
+import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/CategoriesRepository';
 import { DrinksRepositoryInMemory } from '@modules/drinks/repositories/inMemory/DrinksRepository';
 import { IngredientsRepositoryInMemory } from '@modules/drinks/repositories/inMemory/IngredientsRepository';
 import { ObjectId } from 'bson';
 import 'reflect-metadata';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { DeleteDrinkService } from './DeleteDrinkService';
-import { DRINK_ERRORS } from '@modules/drinks/errors/drinkErrors';
 
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 let ingredientsRepositoryInMemory: IngredientsRepositoryInMemory;
 let drinksRepositoryInMemory: DrinksRepositoryInMemory;
 let deleteDrinkService: DeleteDrinkService;
@@ -15,23 +18,33 @@ let deleteDrinkService: DeleteDrinkService;
 // test constants
 const name = 'Drink test name';
 const method = 'drink recept...';
-const testIngredient1: ICreateIngredient = {
-	name: 'Ingredient 1',
-	category: 'Test',
-	unity: 'ml',
-	colorTheme: '#000000',
-	isAlcoholic: true
-};
+const ingredientName = 'Ingredient test';
+const ingredientCategoryName = 'Category test';
+const ingredientUnitySingular = 'ml';
+const ingredientUnityPlural = 'ml';
+const ingredientIsAlcoholic = true;
+let createdCategory: Category;
+let createdIngredient: Ingredient;
 
 describe('Delete Drink', () => {
-	beforeEach(() => {
-		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
+	beforeEach(async () => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory(categoriesRepositoryInMemory);
 		drinksRepositoryInMemory = new DrinksRepositoryInMemory(ingredientsRepositoryInMemory);
 		deleteDrinkService = new DeleteDrinkService(drinksRepositoryInMemory);
+
+		// test constants
+		createdCategory = await categoriesRepositoryInMemory.create({ name: ingredientCategoryName });
+		createdIngredient = await ingredientsRepositoryInMemory.create({
+			name: ingredientName,
+			categoryId: createdCategory.id,
+			unitySingular: ingredientUnitySingular,
+			unityPlural: ingredientUnityPlural,
+			isAlcoholic: ingredientIsAlcoholic
+		});
 	});
 	it('should be able to delete a drink', async () => {
-		const createdIngredient = await ingredientsRepositoryInMemory.create(testIngredient1);
-		let createdDrink = await drinksRepositoryInMemory.create({
+		const createdDrink = await drinksRepositoryInMemory.create({
 			name,
 			method,
 			ingredients: [{ ingredientId: createdIngredient.id, quantity: 60 }]
@@ -39,9 +52,9 @@ describe('Delete Drink', () => {
 
 		await deleteDrinkService.execute({ id: createdDrink.id });
 
-		createdDrink = await drinksRepositoryInMemory.findById(createdDrink.id);
+		const findDeledDrink = await drinksRepositoryInMemory.findById(createdDrink.id);
 
-		expect(createdDrink).toBeUndefined();
+		expect(findDeledDrink).toBeUndefined();
 	});
 
 	it('should not be able to delete a nonexistent drink', async () => {
