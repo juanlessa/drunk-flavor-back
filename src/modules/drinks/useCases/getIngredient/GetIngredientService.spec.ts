06647cc1f20fdb81d7d32c02ae63e48f46195911
Juan Lessa <juanvlessa@ua.pt>
2023-08-05 10:21:46 +0100

    refactor: :recycle: get ingredient using prisma relations

--- src/modules/drinks/useCases/getIngredient/GetIngredientService.spec.ts
+++ src/modules/drinks/useCases/getIngredient/GetIngredientService.spec.ts
@@ -1,42 +1,50 @@
 import AppError from '@errors/AppError';
 import { INGREDIENT_ERRORS } from '@modules/drinks/errors/ingredientErrors';
+import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/CategoriesRepository';
 import { IngredientsRepositoryInMemory } from '@modules/drinks/repositories/inMemory/IngredientsRepository';
+import { Category } from '@prisma/client';
 import { ObjectId } from 'bson';
 import 'reflect-metadata';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { GetIngredientService } from './GetIngredientService';
 
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 let ingredientsRepositoryInMemory: IngredientsRepositoryInMemory;
 let getIngredientService: GetIngredientService;
 
 // test constants
 const name = 'Ingredient test';
-const category = 'test';
-const unity = 'ml';
-const colorTheme = '#000000';
+const categoryName = 'Category test';
+const unitySingular = 'ml';
+const unityPlural = 'ml';
 const isAlcoholic = true;
+let createdCategory: Category;
 
 describe('Get Ingredient', () => {
-	beforeEach(() => {
-		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
+	beforeEach(async () => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory(categoriesRepositoryInMemory);
 		getIngredientService = new GetIngredientService(ingredientsRepositoryInMemory);
+
+		// test constants
+		createdCategory = await categoriesRepositoryInMemory.create({ name: categoryName });
 	});
 	it('should be able to find an ingredient', async () => {
 		const createdIngredient = await ingredientsRepositoryInMemory.create({
 			name,
-			category,
-			unity,
-			isAlcoholic,
-			colorTheme
+			categoryId: createdCategory.id,
+			unitySingular,
+			unityPlural,
+			isAlcoholic
 		});
 
 		const ingredientFound = await getIngredientService.execute({ id: createdIngredient.id });
 
 		expect(ingredientFound.id).toEqual(createdIngredient.id);
 		expect(ingredientFound.name).toEqual(createdIngredient.name);
-		expect(ingredientFound.category).toEqual(createdIngredient.category);
-		expect(ingredientFound.unity).toEqual(createdIngredient.unity);
-		expect(ingredientFound.colorTheme).toEqual(createdIngredient.colorTheme);
+		expect(ingredientFound.categoryId).toEqual(createdIngredient.categoryId);
+		expect(ingredientFound.unitySingular).toEqual(createdIngredient.unitySingular);
+		expect(ingredientFound.unityPlural).toEqual(createdIngredient.unityPlural);
 		expect(ingredientFound.isAlcoholic).toEqual(createdIngredient.isAlcoholic);
 	});
 
