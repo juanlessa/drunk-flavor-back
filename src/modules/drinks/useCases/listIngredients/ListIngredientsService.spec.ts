fc14994e25f9640227b75524552faed7e74e2a84
Juan Lessa <juanvlessa@ua.pt>
2023-08-05 10:22:38 +0100

    refactor: :recycle: list ingredients using prisma relations

--- src/modules/drinks/useCases/listIngredients/ListIngredientsService.spec.ts
+++ src/modules/drinks/useCases/listIngredients/ListIngredientsService.spec.ts
@@ -1,31 +1,39 @@
+import Category from '@modules/drinks/entities/Category';
+import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/CategoriesRepository';
 import { IngredientsRepositoryInMemory } from '@modules/drinks/repositories/inMemory/IngredientsRepository';
 import 'reflect-metadata';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ListIngredientsService } from './ListIngredientsService';
 
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 let ingredientsRepositoryInMemory: IngredientsRepositoryInMemory;
 let listIngredientsService: ListIngredientsService;
 
 // test constants
 const name = 'Ingredient test';
-const category = 'test';
-const unity = 'ml';
-const colorTheme = '#000000';
+const categoryName = 'Category test';
+const unitySingular = 'ml';
+const unityPlural = 'ml';
 const isAlcoholic = true;
+let createdCategory: Category;
 
 describe('List Ingredients', () => {
-	beforeEach(() => {
-		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
+	beforeEach(async () => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory(categoriesRepositoryInMemory);
 		listIngredientsService = new ListIngredientsService(ingredientsRepositoryInMemory);
+
+		// test constants
+		createdCategory = await categoriesRepositoryInMemory.create({ name: categoryName });
 	});
 
 	it('should be able to list all ingredients', async () => {
-		const createdIngredient = await ingredientsRepositoryInMemory.create({
+		await ingredientsRepositoryInMemory.create({
 			name,
-			category,
-			unity,
-			isAlcoholic,
-			colorTheme
+			categoryId: createdCategory.id,
+			unitySingular,
+			unityPlural,
+			isAlcoholic
 		});
 
 		const ingredientsFound = await listIngredientsService.execute();
