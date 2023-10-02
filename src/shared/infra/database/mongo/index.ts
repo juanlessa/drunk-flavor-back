046003a8beb254d6f457e47adf522f407a411d21
Juan Lessa <juanvlessa@ua.pt>
2023-10-02 22:31:51 +0100

    refactor: :recycle: mongo

---
+++ src/shared/infra/database/mongo/index.ts
@@ -0,0 +1,30 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+let mongoClient: Mongoose;
+
+export async function initiateMongo(): Promise<void> {
+	const mongoHost = process.env.MONGO_HOST || 'localhost';
+	const mongoPort = process.env.MONGO_PORT || '27017';
+	const mongoMaxPoolSize = process.env.MONGO_MAX_POOL_SIZE || '25';
+	const mongoDb = process.env.MONGO_DATABASE || 'drunkflavor';
+	const mongoAuthDb = process.env.MONGO_AUTH_DB || 'admin';
+	const mongoUser = process.env.MONGO_USERNAME || '';
+	const mongoPwd = process.env.MONGO_PASSWORD;
+	//const params = `authMechanism=SCRAM-SHA-256&authSource=${mongoAuthDb}`;
+	const params = ``;
+
+	try {
+		mongoClient = await mongoose.connect(
+			`mongodb://${mongoUser}:${mongoPwd}@${mongoHost}:${mongoPort}/${mongoDb}?${params}`,
+			{ maxPoolSize: parseInt(mongoMaxPoolSize), serverSelectionTimeoutMS: 5000, connectTimeoutMS: 5000 }
+		);
+		console.log('[INFO] Mongo connection has been stablish.');
+	} catch (error) {
+		console.error('Could not connect ', error);
+		throw new Error(error);
+	}
+}
+
+export function getMongoClient(): Mongoose {
+	return mongoClient;
+}
