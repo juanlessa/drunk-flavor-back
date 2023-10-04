442933b4ce3c2cc438b26c4783d7c17d3d49f353
Juan Lessa <juanvlessa@ua.pt>
2023-10-04 21:08:52 +0100

    refactor: :recycle: update imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
 import '@shared/container/providers';
-import { IIngredientsRepository } from '@modules/drinks/repositories/ingredients.repository.interface';
-import { IngredientsRepository } from '@modules/drinks/infra/mongo/repositories/ingredients.repository';
-import { ICategoriesRepository } from '@modules/drinks/repositories/categories.repository.interface';
-import { CategoriesRepository } from '@modules/drinks/infra/mongo/repositories/categories.repository';
-import { IDrinksRepository } from '@modules/drinks/repositories/drinks.repository.interface';
-import { DrinksRepository } from '@modules/drinks/infra/mongo/repositories/drinks.repository';
+import { IIngredientsRepository } from '@modules/drinks/repositories/IIngredients.repository';
+import { IngredientsRepository } from '@modules/drinks/infra/mongo/repositories/Ingredients.repository';
+import { ICategoriesRepository } from '@modules/drinks/repositories/ICategories.repository';
+import { CategoriesRepository } from '@modules/drinks/infra/mongo/repositories/Categories.repository';
+import { IDrinksRepository } from '@modules/drinks/repositories/IDrinks.repository';
+import { DrinksRepository } from '@modules/drinks/infra/mongo/repositories/Drinks.repository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '@modules/accounts/infra/mongo/repositories/UsersRepository';
 import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
