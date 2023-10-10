c4f0f15a3139f575807f57e18177a2761f29af1b
Juan Lessa <juanvlessa@ua.pt>
2023-10-10 22:48:06 +0100

    refactor: :recycle: functional user mapper

--- src/modules/accounts/mappers/user.mapper.ts
+++ src/modules/accounts/mappers/user.mapper.ts
@@ -1,7 +1,15 @@
 import { IUserProfileResponse } from '@modules/accounts/dtos/user.dtos';
-import { User } from '@modules/accounts/entities/user.entity';
+import { IUser } from '@modules/accounts/entities/user.entity';
 
-export const mapToUserDto = ({ email, name, surname, _id, role }: User): IUserProfileResponse => {
-	const userProfile: IUserProfileResponse = { _id, name, surname, email, role };
+export const mapToUserDto = ({
+	_id,
+	name,
+	surname,
+	email,
+	role,
+	created_at,
+	updated_at
+}: IUser): IUserProfileResponse => {
+	const userProfile: IUserProfileResponse = { _id, name, surname, email, role, created_at, updated_at };
 	return userProfile;
 };
