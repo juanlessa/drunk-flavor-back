919c5df46303233231742ec80b01e268e1e691e6
Juan Lessa <juanvlessa@ua.pt>
2023-10-11 11:38:46 +0100

    refactor: :recycle: refactor unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
 	test: {
 		include: ['src/**/*.spec.ts'],
-		exclude: ['src/**/*Controller.spec.ts']
+		exclude: ['src/**/*.e2e-spec.ts']
 	},
 	plugins: [tsconfigPaths()]
 });
