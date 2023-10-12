03ac2474cac4fc9c7d2551f1f4c58dff9e50839d
Juan Lessa <juanvlessa@ua.pt>
2023-10-12 11:49:16 +0100

    refactor: :recycle: delete ingredient tests refector

--- src/modules/drinks/useCases/deleteIngredient/deleteIngredient.spec.ts
+++ src/modules/drinks/useCases/deleteIngredient/deleteIngredient.spec.ts
@@ -1,46 +1,52 @@
 import AppError from '@errors/AppError';
-import Category from '@modules/drinks/entities/category.entity';
+import { ICategory, ICategoryTranslation } from '@modules/drinks/entities/category.entity';
 import { INGREDIENT_ERRORS } from '@modules/drinks/errors/ingredient.errors';
-import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/CategoriesRepository';
+import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/Categories.repository';
 import { IngredientsRepositoryInMemory } from '@modules/drinks/repositories/inMemory/IngredientsRepository';
 import { ObjectId } from 'bson';
 import 'reflect-metadata';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { DeleteIngredientService } from './deleteIngredient.service';
+import { DeleteIngredientService } from './DeleteIngredient.service';
+import { ITranslations } from '@modules/drinks/types/translations';
+import { IIngredientTranslation } from '@modules/drinks/entities/ingredient.entity';
 
 let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 let ingredientsRepositoryInMemory: IngredientsRepositoryInMemory;
 let deleteIngredientService: DeleteIngredientService;
 
 // test constants
-const name = 'Ingredient test';
-const categoryName = 'Category test';
-const unitySingular = 'ml';
-const unityPlural = 'ml';
+const translations: ITranslations<IIngredientTranslation> = {
+	en: { name: 'en name', unit: 'ml', unit_plural: 'ml' },
+	pt: { name: 'pt name', unit: 'ml', unit_plural: 'ml' }
+};
 const isAlcoholic = true;
-let createdCategory: Category;
+const categoryTranslations: ITranslations<ICategoryTranslation> = {
+	en: {
+		name: 'category en name'
+	},
+	pt: { name: 'category pt name' }
+};
+let createdCategory: ICategory;
 
 describe('Delete Ingredient', () => {
 	beforeEach(async () => {
 		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
-		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory(categoriesRepositoryInMemory);
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
 		deleteIngredientService = new DeleteIngredientService(ingredientsRepositoryInMemory);
 
 		// test constants
-		createdCategory = await categoriesRepositoryInMemory.create({ name: categoryName });
+		createdCategory = await categoriesRepositoryInMemory.create({ translations: categoryTranslations });
 	});
 	it('should be able to delete an ingredient', async () => {
 		const createdIngredient = await ingredientsRepositoryInMemory.create({
-			name,
-			categoryId: createdCategory.id,
-			unitySingular,
-			unityPlural,
-			isAlcoholic
+			translations,
+			category: createdCategory,
+			is_alcoholic: isAlcoholic
 		});
 
-		await deleteIngredientService.execute({ id: createdIngredient.id });
+		await deleteIngredientService.execute({ id: createdIngredient._id });
 
-		const findDeledIngredient = await ingredientsRepositoryInMemory.findById(createdIngredient.id);
+		const findDeledIngredient = await ingredientsRepositoryInMemory.findById(createdIngredient._id);
 
 		expect(findDeledIngredient).toBeUndefined();
 	});
