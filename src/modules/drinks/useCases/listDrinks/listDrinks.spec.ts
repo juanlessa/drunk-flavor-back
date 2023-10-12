26a3d6e067527e0f9c8c7315bb14e7d198979e98
Juan Lessa <juanvlessa@ua.pt>
2023-10-12 12:17:26 +0100

    refactor: :recycle: list drinks tests

---
+++ src/modules/drinks/useCases/listDrinks/listDrinks.spec.ts
@@ -0,0 +1,63 @@
+import { ICategory, ICategoryTranslation } from '@modules/drinks/entities/category.entity';
+import { IIngredient, IIngredientTranslation } from '@modules/drinks/entities/ingredient.entity';
+import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/Categories.repository';
+import { DrinksRepositoryInMemory } from '@modules/drinks/repositories/inMemory/DrinksRepository';
+import { IngredientsRepositoryInMemory } from '@modules/drinks/repositories/inMemory/IngredientsRepository';
+import 'reflect-metadata';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { ListDrinksService } from './ListDrinks.service';
+import { ITranslations } from '@modules/drinks/types/translations';
+import { IDrinkTranslation } from '@modules/drinks/entities/drink.entity';
+
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let ingredientsRepositoryInMemory: IngredientsRepositoryInMemory;
+let drinksRepositoryInMemory: DrinksRepositoryInMemory;
+let listDrinksService: ListDrinksService;
+
+// test constants
+const translations: ITranslations<IDrinkTranslation> = {
+	en: { name: 'en name', method: 'en method...' },
+	pt: { name: 'pt name', method: 'pt method...' }
+};
+
+const ingredientTranslations: ITranslations<IIngredientTranslation> = {
+	en: { name: 'en name', unit: 'ml', unit_plural: 'ml' },
+	pt: { name: 'pt name', unit: 'ml', unit_plural: 'ml' }
+};
+const ingredientIsAlcoholic = true;
+let createdIngredient: IIngredient;
+const categoryTranslations: ITranslations<ICategoryTranslation> = {
+	en: {
+		name: 'category en name'
+	},
+	pt: { name: 'category pt name' }
+};
+let createdCategory: ICategory;
+
+describe('List Drinks', () => {
+	beforeEach(async () => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
+		drinksRepositoryInMemory = new DrinksRepositoryInMemory();
+		listDrinksService = new ListDrinksService(drinksRepositoryInMemory);
+
+		// test constants
+		createdCategory = await categoriesRepositoryInMemory.create({ translations: categoryTranslations });
+		createdIngredient = await ingredientsRepositoryInMemory.create({
+			translations: ingredientTranslations,
+			category: createdCategory,
+			is_alcoholic: ingredientIsAlcoholic
+		});
+	});
+
+	it('should be able to list all drinks', async () => {
+		await drinksRepositoryInMemory.create({
+			translations,
+			ingredients: [{ ingredient: createdIngredient, quantity: 60 }]
+		});
+
+		const drinksFound = await listDrinksService.execute();
+
+		expect(drinksFound.length).toEqual(1);
+	});
+});
