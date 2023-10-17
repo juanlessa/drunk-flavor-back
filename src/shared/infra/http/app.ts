fd90ebf147095d2f13da265a4fd2228e4696f07c
Juan Lessa <juanvlessa@ua.pt>
2023-10-17 01:14:18 +0100

    refactor: :recycle: change errorHandler path

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,7 +8,7 @@ import routes from '@routes/index';
 import swaggerFile from '../../../swagger.json';
 import swaggerUi from 'swagger-ui-express';
 import '@shared/container';
-import { errorHandler } from '@errors/errorHandler';
+import { errorHandler } from '@middlewares/errorHandler';
 import { initiateMongo } from '@shared/infra/database/mongo';
 
 dotenv.config();
