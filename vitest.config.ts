cf93ccaddb2e99899c7814ecea6f220f318e7ce4
Juan Lessa <juanvlessa@ua.pt>
2023-10-28 00:40:32 +0100

    refactor: :recycle: remove vitest alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
 	test: {
 		include: ['src/**/*.spec.ts'],
-		exclude: ['src/**/*.e2e-spec.ts'],
-		alias: {
-			'@config': new URL('./src/config', import.meta.url).pathname,
-			'@modules': new URL('./src/modules', import.meta.url).pathname,
-			'@shared': new URL('./src/shared', import.meta.url).pathname,
-			'@utils': new URL('./src/utils', import.meta.url).pathname
-		}
+		exclude: ['src/**/*.e2e-spec.ts']
 	},
 	plugins: [tsconfigPaths()]
 });
