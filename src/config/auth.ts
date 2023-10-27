461b87fcffa4a509c2ed6a2b57bd3ea207135e10
Juan Lessa <juanvlessa@ua.pt>
2023-10-27 22:14:45 +0100

    refactor: :recycle: set default secrets

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,10 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-	secret_token: process.env.TOKEN_SECRET,
-	expires_in_token: process.env.TOKEN_EXPIRES_IN,
-	expires_token_hours: Number(process.env.TOKEN_EXPIRES_HOURS),
-	secret_refresh_token: process.env.REFRESH_TOKEN_SECRET,
-	expires_in_refresh_token: process.env.REFRESH_TOKEN_EXPIRES_IN,
-	expires_refresh_token_days: Number(process.env.REFRESH_TOKEN_EXPIRES_DAYS)
+	secret_token: process.env.TOKEN_SECRET || 'test-token-secret',
+	expires_in_token: process.env.TOKEN_EXPIRES_IN || '1h',
+	expires_token_hours: Number(process.env.TOKEN_EXPIRES_HOURS) || 1,
+	secret_refresh_token: process.env.REFRESH_TOKEN_SECRET || 'test-refresh-token-secret',
+	expires_in_refresh_token: process.env.REFRESH_TOKEN_EXPIRES_IN || '1d',
+	expires_refresh_token_days: Number(process.env.REFRESH_TOKEN_EXPIRES_DAYS) || 1
 };
