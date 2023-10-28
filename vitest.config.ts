48020890429b2af77fa6f2dfacc48f03f51339d5
Juan Lessa <juanvlessa@ua.pt>
2023-10-28 02:20:48 +0100

    refactor: :recycle: change alias approach

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,12 @@ export default defineConfig({
 	test: {
 		root: resolve(__dirname),
 		include: ['src/**/*.spec.ts'],
-		exclude: ['src/**/*.e2e-spec.ts']
-	},
-	plugins: [tsconfigPaths()]
+		exclude: ['src/**/*.e2e-spec.ts'],
+		alias: [
+			{ find: '@config', replacement: resolve(__dirname, 'src/config/') },
+			{ find: '@modules', replacement: resolve(__dirname, 'src/modules/') },
+			{ find: '@shared', replacement: resolve(__dirname, 'src/shared/') },
+			{ find: '@utils', replacement: resolve(__dirname, 'src/utils/') }
+		]
+	}
 });
