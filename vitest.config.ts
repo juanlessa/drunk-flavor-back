ce031209d10e28a5fbb8d30b3f3d258ef40ae8fa
Juan Lessa <juanvlessa@ua.pt>
2023-10-28 14:31:36 +0100

    refactor: :recycle: update path resolve

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,10 @@
-import { resolve } from 'path';
-
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	test: {
-		root: resolve(__dirname),
 		include: ['src/**/*.spec.ts'],
-		exclude: ['src/**/*.e2e-spec.ts'],
-		alias: [
-			{ find: '@config', replacement: resolve(__dirname, 'src/config/') },
-			{ find: '@modules', replacement: resolve(__dirname, 'src/modules/') },
-			{ find: '@shared', replacement: resolve(__dirname, 'src/shared/') },
-			{ find: '@utils', replacement: resolve(__dirname, 'src/utils/') }
-		]
-	}
+		exclude: ['src/**/*.e2e-spec.ts']
+	},
+	plugins: [tsconfigPaths()]
 });
