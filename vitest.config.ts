2d567b7322d849bd5bba6761c28c36141535c52e
Juan Lessa <juanvlessa@ua.pt>
2023-10-28 01:32:54 +0100

    feat: :sparkles: coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
+import { resolve } from 'path';
+
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	test: {
+		root: resolve(__dirname),
 		include: ['src/**/*.spec.ts'],
 		exclude: ['src/**/*.e2e-spec.ts']
 	},
