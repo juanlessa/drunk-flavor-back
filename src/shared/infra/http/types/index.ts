2b4ab51efd3636e68e7bc027a987616ccbc0d68b
Juan Lessa <juanvlessa@ua.pt>
2023-10-29 11:59:13 +0000

    feat: :sparkles: express override request

--- src/shared/infra/http/types/index.ts
+++ src/shared/infra/http/types/index.ts
@@ -1,10 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 
-export type AppRequest = Request & {
-	user: {
-		id: string;
-	};
-	file?: Express.Multer.File & Express.MulterS3.File;
-};
+export type AppRequest = Request;
 export type AppResponse = Response;
 export type AppNextFunction = NextFunction;
