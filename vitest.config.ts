7bf0ac2188bdcbf177385f5d0e6e2da44255f02d
Juan Lessa <juanvlessa@ua.pt>
2023-10-30 11:37:56 +0000

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	test: {
-		include: ['src/**/*.spec.ts'],
-		exclude: ['src/**/*.e2e-spec.ts']
-	},
-	plugins: [tsconfigPaths()]
+	plugins: [tsconfigPaths({ loose: true })]
 });
