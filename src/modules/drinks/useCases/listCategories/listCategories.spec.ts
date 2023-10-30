427697cba0b5fc84ab05daf07c973b17637158d5
Juan Lessa <juanvlessa@ua.pt>
2023-10-30 19:46:48 +0000

    refactor: :recycle: refact list category tests

--- src/modules/drinks/useCases/listCategories/listCategories.spec.ts
+++ src/modules/drinks/useCases/listCategories/listCategories.spec.ts
@@ -1,7 +1,7 @@
+import { ICategory, ICategoryTranslation } from '@modules/drinks/entities/category.entity';
 import { CategoriesRepositoryInMemory } from '@modules/drinks/repositories/inMemory/Categories.repository';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ListCategoriesService } from '@modules/drinks/useCases/listCategories/ListCategories.service';
-import { ICategoryTranslation } from '@modules/drinks/entities/category.entity';
 import { ITranslations } from '@modules/drinks/types/translations';
 
 let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
@@ -9,21 +9,22 @@ let listCategoriesService: ListCategoriesService;
 
 // test constants
 const translations: ITranslations<ICategoryTranslation> = {
-	en: {
-		name: 'en name'
-	},
+	en: { name: 'en name' },
 	pt: { name: 'pt name' }
 };
 
-describe('List categories', () => {
-	beforeEach(() => {
+let createdCategory: ICategory;
+
+describe('List Categories', () => {
+	beforeEach(async () => {
 		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
 		listCategoriesService = new ListCategoriesService(categoriesRepositoryInMemory);
+
+		// test constants
+		createdCategory = await categoriesRepositoryInMemory.create({ translations });
 	});
 
 	it('should be able to list all categories', async () => {
-		await categoriesRepositoryInMemory.create({ translations });
-
 		const categoriesFound = await listCategoriesService.execute();
 
 		expect(categoriesFound.length).toEqual(1);
