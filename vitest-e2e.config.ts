b7b70f8f50274c8d310b041a834421c9a48d5154
Juan Lessa <juanvlessa@ua.pt>
2023-11-01 22:23:17 +0000

    refactor: :recycle: update vitest config

--- vitest-e2e.config.ts
+++ vitest-e2e.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
+	root: fileURLToPath(new URL('./', import.meta.url)),
 	test: {
 		include: ['src/**/*.e2e-spec.ts'],
-		threads: false
+		threads: false,
+		environment: 'node',
+		globals: true
 	},
-	plugins: [tsconfigPaths()]
+	resolve: {
+		alias: [
+			{ find: '@config', replacement: fileURLToPath(new URL('./src/config', import.meta.url)) },
+			{ find: '@modules', replacement: fileURLToPath(new URL('./src/modules', import.meta.url)) },
+			{ find: '@shared', replacement: fileURLToPath(new URL('./src/shared', import.meta.url)) },
+			{ find: '@utils', replacement: fileURLToPath(new URL('./src/utils', import.meta.url)) }
+		]
+	}
 });
