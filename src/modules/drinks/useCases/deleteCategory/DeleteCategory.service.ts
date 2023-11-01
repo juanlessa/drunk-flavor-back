a6e9213cf464680391c752ab6537e6b46ece33a3
Juan Lessa <juanvlessa@ua.pt>
2023-11-01 21:40:06 +0000

    fix: :bug: service with correct name

---
+++ src/modules/drinks/useCases/deleteCategory/DeleteCategory.service.ts
@@ -0,0 +1,19 @@
+import { IDeleteCategory } from '@modules/drinks/dtos/category.dtos';
+import { CATEGORY_ERRORS } from '@modules/drinks/errors/category.errors';
+import { ICategoriesRepository } from '@modules/drinks/repositories/ICategories.repository';
+import AppError from '@shared/errors/AppError';
+
+class DeleteCategoryService {
+	constructor(private categoriesRepository: ICategoriesRepository) {}
+
+	async execute({ id }: IDeleteCategory): Promise<void> {
+		const categoryExists = await this.categoriesRepository.findById(id);
+		if (!categoryExists) {
+			throw new AppError(CATEGORY_ERRORS.not_exist);
+		}
+
+		await this.categoriesRepository.delete(id);
+	}
+}
+
+export { DeleteCategoryService };
