5418561af9ead6a764a3873ffe1eb9985e62bd20
Juan Lessa <juanvlessa@ua.pt>
2023-11-01 21:54:17 +0000

    fix: :bug: change names

---
+++ src/modules/drinks/useCases/createIngredient/CreateIngredient.controller.ts
@@ -0,0 +1,17 @@
+import { ICreateIngredientRequest } from '@modules/drinks/dtos/ingredient.dtos';
+import { resolveCreateIngredientService } from '@modules/drinks/useCases/createIngredient/createIngredient.container';
+import { AppRequest, AppResponse } from '@shared/infra/http/types';
+
+class CreateIngredientController {
+	async handle(request: AppRequest, response: AppResponse): Promise<AppResponse> {
+		const { translations, category_id, is_alcoholic }: ICreateIngredientRequest = request.body;
+
+		const service = resolveCreateIngredientService();
+
+		await service.execute({ translations, category_id, is_alcoholic });
+
+		return response.status(201).send();
+	}
+}
+
+export { CreateIngredientController };
