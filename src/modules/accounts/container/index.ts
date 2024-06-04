418afe78a12f3b5d31f22acba8d05afe7ebdf435
juan lessa <juan.lessa@bolttech.io>
2024-06-04 11:13:26 +0100

    refactor: :fire: remove userTokens collection and refreshToken route

--- src/modules/accounts/container/index.ts
+++ src/modules/accounts/container/index.ts
@@ -1,11 +1,6 @@
 import { UsersRepository } from '@modules/accounts/infra/mongo/repositories/Users.repository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsers.repository';
-import { UsersTokensRepository } from '@modules/accounts/infra/mongo/repositories/UsersTokens.repository';
-import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokens.repository';
 
 // repositories
 const usersRepository: IUsersRepository = new UsersRepository();
 export const resolveUsersRepository = () => usersRepository;
-
-const usersTokensRepository: IUsersTokensRepository = new UsersTokensRepository();
-export const resolveUsersTokensRepository = () => usersTokensRepository;
