5fa4b6031ffb17812ca579089402bbc7964844e8
juan lessa <juan.lessa@bolttech.io>
2024-07-03 00:38:21 +0100

    feat: :sparkles: signOut middleware

---
+++ src/shared/infra/fastify/middlewares/signOut.ts
@@ -0,0 +1,7 @@
+import { Middleware } from "../types/fastify.types";
+
+export const signOut: Middleware = async (request, reply) => {
+  request.session.delete();
+  void reply.clearCookie("Authorization");
+  return { ok: true };
+};
