9f65d53adf0e2bb33d1215f9d8611062a31ec378
juan lessa <juan.lessa@bolttech.io>
2024-07-03 00:29:54 +0100

    refactor: :recycle: fastify jwt

--- src/shared/infra/fastify/constants/jwt.constants.ts
+++ src/shared/infra/fastify/constants/jwt.constants.ts
@@ -1,9 +1,14 @@
 import { env } from "@/env";
 import { FastifyJWTOptions } from "@fastify/jwt";
+import { AUTH_COOKIE } from "./cookie.constants";
 
 export const TOKEN_OPTIONS = {
   secret: env.TOKEN_SECRET,
   sign: {
     expiresIn: env.ACCESS_TOKEN_EXPIRES_IN_SECONDS,
+  },
+  cookie: {
+    cookieName: AUTH_COOKIE,
+    signed: true,
   },
 } satisfies FastifyJWTOptions;
---
+++ src/shared/infra/fastify/errors/fastifyJwtError.ts
@@ -0,0 +1,106 @@
+import { FastifyError } from "fastify";
+import { instanceOfFastifyError } from "./fastifyError";
+import { FastifyJwtError, JwtErrorsEnum } from "../types/jwt.types";
+
+export const instanceOfJwtAssertionError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code === JwtErrorsEnum.ERR_ASSERTION
+  );
+};
+
+export const instanceOfJwtBadRequestError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code === JwtErrorsEnum.FST_JWT_BAD_REQUEST
+  );
+};
+
+export const instanceOfJwtBadCookieRequestError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code === JwtErrorsEnum.FST_JWT_BAD_COOKIE_REQUEST
+  );
+};
+
+export const instanceOfJwtNoAuthorizationInHeaderError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code ===
+      JwtErrorsEnum.FST_JWT_NO_AUTHORIZATION_IN_HEADER
+  );
+};
+
+export const instanceOfJwtNoAuthorizationInCookieError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code ===
+      JwtErrorsEnum.FST_JWT_NO_AUTHORIZATION_IN_COOKIE
+  );
+};
+
+export const instanceOfJwtAuthorizationTokenExpiredError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code ===
+      JwtErrorsEnum.FST_JWT_AUTHORIZATION_TOKEN_EXPIRED
+  );
+};
+
+export const instanceOfJwtAuthorizationTokenInvalidError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code ===
+      JwtErrorsEnum.FST_JWT_AUTHORIZATION_TOKEN_INVALID
+  );
+};
+
+export const instanceOfJwtAuthorizationTokenUntrustedError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code ===
+      JwtErrorsEnum.FST_JWT_AUTHORIZATION_TOKEN_UNTRUSTED
+  );
+};
+
+export const instanceOfJwtMissingSignatureError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (error as FastifyError).code === JwtErrorsEnum.FAST_JWT_MISSING_SIGNATURE
+  );
+};
+
+export const instanceOfFastifyJwtError = (
+  error: unknown
+): error is FastifyJwtError => {
+  return (
+    instanceOfFastifyError(error) &&
+    (instanceOfJwtAssertionError(error) ||
+      instanceOfJwtBadRequestError(error) ||
+      instanceOfJwtBadCookieRequestError(error) ||
+      instanceOfJwtNoAuthorizationInHeaderError(error) ||
+      instanceOfJwtNoAuthorizationInCookieError(error) ||
+      instanceOfJwtAuthorizationTokenExpiredError(error) ||
+      instanceOfJwtAuthorizationTokenInvalidError(error) ||
+      instanceOfJwtAuthorizationTokenUntrustedError(error) ||
+      instanceOfJwtMissingSignatureError(error))
+  );
+};
