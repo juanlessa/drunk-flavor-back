52d66bf4641b830cb20a89f8873cd5466e4448b4
juan lessa <juan.lessa@bolttech.io>
2024-07-02 03:49:56 +0100

    feat: :tada: configuration files

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	root: fileURLToPath(new URL('./', import.meta.url)),
+	test: {
+		include: ['src/**/*.spec.ts'],
+		exclude: ['src/**/*.e2e-spec.ts'],
+		environment: 'node'
+	}
+});
