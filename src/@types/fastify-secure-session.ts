2bfaeba708ef4076c945008dc18438e4f1f6575e
juan lessa <juan.lessa@bolttech.io>
2024-07-03 14:54:29 +0100

    refactor: :recycle: import fastify secure sessions

--- src/@types/fastify-secure-session.ts
+++ src/@types/fastify-secure-session.ts
@@ -1,3 +1,4 @@
+import "@fastify/secure-session";
 import { AuthSessionData } from "@/shared/infra/fastify/types/session.types";
 import { AUTH_SESSION } from "@/shared/infra/fastify/constants/session.constants";
 
