3a5bae63530397bab0888a0556c0abd98864752a
juan lessa <juan.lessa@bolttech.io>
2024-07-06 23:33:26 +0100

    refactor: :recycle: include delete and list users handlers

--- src/shared/infra/fastify/routes/users.routes.ts
+++ src/shared/infra/fastify/routes/users.routes.ts
@@ -5,13 +5,26 @@ import { pluginGenerator } from '../helpers/fastify.helpers';
 import { Routes } from '../types/fastify.types';
 import { verifyAndRenewToken } from '../middlewares/verifyAndRenewToken';
 import { getUserProfileController } from '@/modules/accounts/useCases/getUserProfile/getUserProfile.controller';
+import { deleteUserSchema } from '@/modules/accounts/useCases/deleteUser/deleteUser.schema';
+import { deleteUserController } from '@/modules/accounts/useCases/deleteUser/deleteUser.controller';
+import { listUsersController } from '@/modules/accounts/useCases/listUsers/listUsers.controller';
 
 const routes: Routes = (server) => {
 	server
 		.withTypeProvider<ZodTypeProvider>()
 		.post('/users', { schema: { body: createUserSchema }, onRequest: [verifyAndRenewToken] }, createUserController);
 
-	server.post('/users/me', { onRequest: [verifyAndRenewToken] }, getUserProfileController);
+	server.get('/users/me', { onRequest: [verifyAndRenewToken] }, getUserProfileController);
+
+	server.get('/users', { onRequest: [verifyAndRenewToken] }, listUsersController);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.delete(
+			'/users',
+			{ schema: { body: deleteUserSchema }, onRequest: [verifyAndRenewToken] },
+			deleteUserController,
+		);
 };
 
 export const usersRoutes = pluginGenerator(routes);
