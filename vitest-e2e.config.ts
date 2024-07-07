d70ecda602d1a347870d9b3fdca9e8c4bd05449d
juan lessa <juan.lessa@bolttech.io>
2024-07-07 23:36:20 +0100

    feat: :sparkles: e2e test config

---
+++ vitest-e2e.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	root: fileURLToPath(new URL('./', import.meta.url)),
+	test: {
+		include: ['src/**/*.e2e-spec.ts'],
+		environment: 'mongo',
+	},
+});
