56aa3cc59fca0519b6bddcfe6dc785b054e18d17
juan lessa <juan.lessa@bolttech.io>
2024-07-07 20:53:02 +0100

    feat: :sparkles: add categories routes

---
+++ src/shared/infra/fastify/routes/categories.routes.ts
@@ -0,0 +1,53 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { pluginGenerator } from '../helpers/fastify.helpers';
+import { Routes } from '../types/fastify.types';
+import { verifyAndRenewToken } from '../middlewares/verifyAndRenewToken';
+import { createCategorySchema } from '@/modules/drinks/useCases/categories/create/createCategory.schema';
+import { createCategoryController } from '@/modules/drinks/useCases/categories/create/createCategory.controller';
+import { deleteCategorySchema } from '@/modules/drinks/useCases/categories/delete/deleteCategory.schema';
+import { deleteCategoryController } from '@/modules/drinks/useCases/categories/delete/deleteCategory.controller';
+import { getCategorySchema } from '@/modules/drinks/useCases/categories/get/getCategory.schema';
+import { getCategoryController } from '@/modules/drinks/useCases/categories/get/getCategory.controller';
+import { listCategoriesController } from '@/modules/drinks/useCases/categories/list/listCategories.controller';
+import { updateCategorySchema } from '@/modules/drinks/useCases/categories/update/updateCategory.schema';
+import { updateCategoryController } from '@/modules/drinks/useCases/categories/update/updateCategory.controller';
+
+const routes: Routes = (server) => {
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.post(
+			'/categories',
+			{ schema: { body: createCategorySchema }, onRequest: [verifyAndRenewToken] },
+			createCategoryController,
+		);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.get(
+			'/categories/:id',
+			{ schema: { params: getCategorySchema }, onRequest: [verifyAndRenewToken] },
+			getCategoryController,
+		);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.get('/categories', { onRequest: [verifyAndRenewToken] }, listCategoriesController);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.patch(
+			'/categories',
+			{ schema: { body: updateCategorySchema }, onRequest: [verifyAndRenewToken] },
+			updateCategoryController,
+		);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.delete(
+			'/categories',
+			{ schema: { body: deleteCategorySchema }, onRequest: [verifyAndRenewToken] },
+			deleteCategoryController,
+		);
+};
+
+export const categoriesRoutes = pluginGenerator(routes);
--- src/shared/infra/fastify/routes/index.ts
+++ src/shared/infra/fastify/routes/index.ts
@@ -1,4 +1,5 @@
 import { authenticateRoutes } from './authenticate.routes';
+import { categoriesRoutes } from './categories.routes';
 import { usersRoutes } from './users.routes';
 
-export const router = [authenticateRoutes, usersRoutes];
+export const router = [authenticateRoutes, usersRoutes, categoriesRoutes];
