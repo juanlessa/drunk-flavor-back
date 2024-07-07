55f1152324f45494d8302cc41b16511655e42029
juan lessa <juan.lessa@bolttech.io>
2024-07-07 20:49:37 +0100

    refactor: :recycle: drinks api response messages in constant folder

---
+++ src/modules/drinks/constants/drinks.constants.ts
@@ -0,0 +1,82 @@
+export const DRINK_MESSAGES = {
+	notExist: {
+		message: 'Drink does not exist',
+		i18nKey: 'apiResponses.drinks.notExist',
+	},
+	alreadyExist: {
+		message: 'Drink already exists',
+		i18nKey: 'apiResponses.drinks.alreadyExist',
+	},
+	notFound: {
+		message: 'Drink not found',
+		i18nKey: 'apiResponses.drinks.notFound',
+	},
+	requiredId: {
+		message: 'ID is required',
+		i18nKey: 'apiResponses.drinks.requiredId',
+	},
+	invalidIdFormat: {
+		message: 'Invalid ID format',
+		i18nKey: 'apiResponses.drinks.invalidIdFormat',
+	},
+	requiredName: {
+		message: 'Name is required',
+		i18nKey: 'apiResponses.drinks.requiredName',
+	},
+	invalidNameFormat: {
+		message: 'Name must have a minimum of 1 character',
+		i18nKey: 'apiResponses.drinks.invalidNameFormat',
+	},
+	nameAlreadyExist: {
+		message: 'Drink name already exists',
+		i18nKey: 'apiResponses.drinks.nameAlreadyExist',
+	},
+	requiredMethod: {
+		message: 'Method is required',
+		i18nKey: 'apiResponses.drinks.requiredMethod',
+	},
+	invalidMethodFormat: {
+		message: 'Method must have a minimum of 1 character',
+		i18nKey: 'apiResponses.drinks.invalidMethodFormat',
+	},
+	requiredCover: {
+		message: 'Cover file name is required',
+		i18nKey: 'apiResponses.drinks.requiredCover',
+	},
+	invalidCoverFormat: {
+		message: 'Cover file name must have a minimum of 1 character',
+		i18nKey: 'apiResponses.drinks.invalidCoverFormat',
+	},
+	requiredThumbnail: {
+		message: 'Thumbnail file name is required',
+		i18nKey: 'apiResponses.drinks.requiredThumbnail',
+	},
+	invalidThumbnailFormat: {
+		message: 'Thumbnail file name must have a minimum of 1 character',
+		i18nKey: 'apiResponses.drinks.invalidThumbnailFormat',
+	},
+	requiredIngredientId: {
+		message: 'Ingredient ID is required',
+		i18nKey: 'apiResponses.drinks.requiredIngredientId',
+	},
+	invalidIngredientIdFormat: {
+		message: 'Invalid ingredient ID format',
+		i18nKey: 'apiResponses.drinks.invalidIngredientIdFormat',
+	},
+	requiredQuantity: {
+		message: 'Quantity is required',
+		i18nKey: 'apiResponses.drinks.requiredQuantity',
+	},
+	invalidQuantityFormat: {
+		message: 'Quantity must be greater than 0',
+		i18nKey: 'apiResponses.drinks.invalidQuantityFormat',
+	},
+	requiredIngredients: {
+		message: 'Drink must have at least 1 ingredient',
+		i18nKey: 'apiResponses.drinks.requiredIngredients',
+	},
+	someIngredientsNotExist: {
+		message: "Some ingredients don't exist",
+		i18nKey: 'apiResponses.drinks.someIngredientsNotExist',
+	},
+};
