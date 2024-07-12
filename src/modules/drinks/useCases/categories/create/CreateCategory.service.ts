d2c469087ea0c348e69856e0a185357809cb8338
juan lessa <juan.lessa@bolttech.io>
2024-07-12 18:29:20 +0100

    refactor: :recycle: update dto

--- src/modules/drinks/useCases/categories/create/CreateCategory.service.ts
+++ src/modules/drinks/useCases/categories/create/CreateCategory.service.ts
@@ -1,13 +1,13 @@
-import { CreateCategory } from '@/modules/drinks/dtos/category.dtos';
 import { mapToTranslationsName } from '@/modules/drinks/mappers/translations.mappers';
 import { ICategoriesRepository } from '@/modules/drinks/repositories/ICategories.repository';
 import { CATEGORY_MESSAGES } from '@/modules/drinks/constants/categories.constants';
 import { BadRequestError } from '@/shared/error/error.lib';
+import { CreateCategoryDTO } from './createCategory.dtos';
 
 export class CreateCategoryService {
 	constructor(private categoriesRepository: ICategoriesRepository) {}
 
-	async execute({ translations }: CreateCategory) {
+	async execute({ translations }: CreateCategoryDTO) {
 		const translationsName = mapToTranslationsName(translations);
 		const categoryALreadyExists = await this.categoriesRepository.findByName(translationsName);
 		if (categoryALreadyExists) {
--- src/modules/drinks/useCases/categories/get/getCategory.schema.ts
+++ src/modules/drinks/useCases/categories/get/getCategory.schema.ts
@@ -1,5 +1,5 @@
-import { categoryIdValidation } from '@/modules/drinks/schemas/category.schemas';
 import { z } from 'zod';
+import { categoryIdValidation } from '@/modules/drinks/schemas/category.schemas';
 
 export const getCategorySchema = z.object({
 	id: categoryIdValidation,
