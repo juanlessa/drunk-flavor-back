e5c7be3d561cb71467972e03f5daf4d01a707b42
Juan Lessa <juanvlessa@ua.pt>
2024-07-21 19:13:42 +0100

    feat: :sparkles: drinks routes

---
+++ src/infrastructure/fastify/routes/drinks.routes.ts
@@ -0,0 +1,46 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { pluginGenerator } from '../helpers/fastify.helpers';
+import { Routes } from '../types/fastify.types';
+import { verifyAndRenewToken } from '../middlewares/verifyAndRenewToken';
+import { createDrinkSchema } from '@/core/drinks/useCases/drinks/create/createDrink.schema';
+import { createDrinkController } from '@/core/drinks/useCases/drinks/create/createDrink.controller';
+import { listDrinksSchema } from '@/core/drinks/useCases/drinks/list/listDrinks.schema';
+import { listDrinksController } from '@/core/drinks/useCases/drinks/list/listDrinks.controller';
+import { deleteDrinkSchema } from '@/core/drinks/useCases/drinks/delete/deleteDrink.schema';
+import { deleteDrinkController } from '@/core/drinks/useCases/drinks/delete/deleteDrink.controller';
+
+const routes: Routes = (server) => {
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.post(
+			'/drinks',
+			{ schema: { body: createDrinkSchema }, onRequest: [verifyAndRenewToken] },
+			createDrinkController,
+		);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.get('/drinks/:id', { schema: { params: createDrinkSchema } }, createDrinkController);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.get('/drinks', { schema: { querystring: listDrinksSchema } }, listDrinksController);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.patch(
+			'/drinks',
+			{ schema: { body: createDrinkSchema }, onRequest: [verifyAndRenewToken] },
+			createDrinkController,
+		);
+
+	server
+		.withTypeProvider<ZodTypeProvider>()
+		.delete(
+			'/drinks',
+			{ schema: { body: deleteDrinkSchema }, onRequest: [verifyAndRenewToken] },
+			deleteDrinkController,
+		);
+};
+
+export const drinksRoutes = pluginGenerator(routes);
--- src/infrastructure/fastify/routes/index.ts
+++ src/infrastructure/fastify/routes/index.ts
@@ -1,6 +1,7 @@
 import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { ingredientsRoutes } from './ingredients.routes';
+import { drinksRoutes } from './drinks.routes';
 import { usersRoutes } from './users.routes';
 
-export const router = [authenticateRoutes, usersRoutes, categoriesRoutes, ingredientsRoutes];
+export const router = [authenticateRoutes, usersRoutes, categoriesRoutes, ingredientsRoutes, drinksRoutes];
