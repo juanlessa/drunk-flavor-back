7f6b57058282fa38a3802bce28145b9589ca8492
Juan Lessa <juanvlessa@ua.pt>
2024-07-21 19:03:26 +0100

    fix: :bug: fix find all arg type

--- src/core/drinks/repositories/IDrinks.repository.ts
+++ src/core/drinks/repositories/IDrinks.repository.ts
@@ -1,13 +1,13 @@
 import { Drink } from '@/core/drinks/entities/drink.entity';
 import { CreateDrink, UpdateDrink } from '@/core/drinks/dtos/drink.dtos';
-import { FindIngredientByName } from '@/core/drinks/dtos/ingredient.dtos';
+import { FindDrinkByName } from '@/core/drinks/dtos/drink.dtos';
 import { QueryParams } from '@/shared/types/query.types';
 
 export interface IDrinksRepository {
 	create(data: CreateDrink): Promise<Drink>;
 	update(data: UpdateDrink): Promise<Drink>;
 	delete(id: string): Promise<Drink>;
-	findByName(data: FindIngredientByName): Promise<Drink | null>;
+	findByName(data: FindDrinkByName): Promise<Drink | null>;
 	findById(id: string): Promise<Drink | null>;
 	findAll(query: QueryParams): Promise<Drink[]>;
 }
