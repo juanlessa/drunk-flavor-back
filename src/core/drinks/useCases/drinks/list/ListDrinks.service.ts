5479b7a8c56b08d23fded12f124035197f45c94d
Juan Lessa <juanvlessa@ua.pt>
2024-09-27 02:51:35 +0100

    refactor: :recycle: list drink use case adding the file url

--- src/core/drinks/useCases/drinks/list/ListDrinks.service.ts
+++ src/core/drinks/useCases/drinks/list/ListDrinks.service.ts
@@ -1,12 +1,26 @@
 import { IDrinksRepository } from '@/core/drinks/repositories/IDrinks.repository';
 import { ListDrinksDTO } from './listDrinks.dtos';
+import { IStorageProvider } from '@/shared/providers/storage/IStorage.provider';
 
 export class ListDrinksService {
-	constructor(private drinksRepository: IDrinksRepository) {}
+	constructor(
+		private drinksRepository: IDrinksRepository,
+		private storageProvider: IStorageProvider,
+	) {}
 
 	async execute({ query = {} }: ListDrinksDTO) {
 		const drinks = await this.drinksRepository.findAll(query);
 
-		return drinks;
+		const drinksWithImagesURL = drinks.map((d) => {
+			if (d.cover) {
+				d.cover.name = this.storageProvider.getFileURL(d.cover.name);
+			}
+			if (d.thumbnail) {
+				d.thumbnail.name = this.storageProvider.getFileURL(d.thumbnail.name);
+			}
+			return d;
+		});
+
+		return drinksWithImagesURL;
 	}
 }
