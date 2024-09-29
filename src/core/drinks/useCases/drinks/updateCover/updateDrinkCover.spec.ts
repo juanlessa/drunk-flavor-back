bb1b9176d8be6caf122e0b615f329376a2eea4e7
Juan Lessa <juanvlessa@ua.pt>
2024-09-29 09:49:17 +0100

    feat: :sparkles: updateDrinkCover tests

---
+++ src/core/drinks/useCases/drinks/updateCover/updateDrinkCover.spec.ts
@@ -0,0 +1,141 @@
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { IngredientsRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Ingredients.repository';
+import { ObjectId } from 'mongodb';
+import { CategoriesRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Categories.repository';
+import { BadRequestError } from '@/shared/error/error.lib';
+import { ICategoriesRepository } from '@/core/drinks/repositories/ICategories.repository';
+import { IIngredientsRepository } from '@/core/drinks/repositories/IIngredients.repository';
+import { createCategoryFactory, createDrinkFactory, createIngredientFactory } from '@/core/drinks/container';
+import { Ingredient } from '@/core/drinks/entities/ingredient.entity';
+import { IDrinksRepository } from '@/core/drinks/repositories/IDrinks.repository';
+import { DrinksRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Drinks.repository';
+import { Drink } from '@/core/drinks/entities/drink.entity';
+import { UpdateDrinkCoverService } from './UpdateDrinkCover.service';
+import { IStorageProvider } from '@/shared/providers/storage/IStorage.provider';
+import { MockStorageProvider } from '@/shared/providers/storage/implementations/MockStorage.provider';
+import { Readable } from 'node:stream';
+import { hasFileExtension } from '@/shared/helpers/file.helpers';
+import { FileMetadata } from '@/shared/types/file.types';
+
+let categoriesRepositoryInMemory: ICategoriesRepository;
+let ingredientsRepositoryInMemory: IIngredientsRepository;
+let drinksRepositoryInMemory: IDrinksRepository;
+let storageProvider: IStorageProvider;
+let service: UpdateDrinkCoverService;
+
+const { translations: categoryTranslations } = createCategoryFactory();
+const { translations: translationsIngredient } = createIngredientFactory();
+
+const { translations } = createDrinkFactory();
+
+const fileName = 'imageTestingFile.png';
+const fileMimetype = 'image/png';
+
+const mockedFileStream = Object.assign(
+	new Readable({
+		read(size) {
+			this.push(null);
+		},
+	}),
+	{ truncated: false, bytesRead: 1234, pipe: vi.fn(), on: vi.fn() },
+);
+
+let ingredient: Ingredient;
+let createdDrink: Drink;
+
+describe('Update Drink', () => {
+	beforeEach(async () => {
+		vi.clearAllMocks();
+
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
+		drinksRepositoryInMemory = new DrinksRepositoryInMemory();
+		storageProvider = new MockStorageProvider();
+		service = new UpdateDrinkCoverService(drinksRepositoryInMemory, storageProvider);
+
+		const category = await categoriesRepositoryInMemory.create({ translations: categoryTranslations });
+		ingredient = await ingredientsRepositoryInMemory.create({
+			translations: translationsIngredient,
+			is_alcoholic: false,
+			category: category,
+		});
+		createdDrink = await drinksRepositoryInMemory.create({
+			translations,
+			ingredients: [{ quantity: 30, ingredient }],
+		});
+	});
+
+	afterAll(async () => {
+		vi.clearAllMocks();
+	});
+
+	it('should be able to update a drink cover', async () => {
+		await service.execute({
+			drinkId: createdDrink._id.toString(),
+			name: fileName,
+			mimetype: fileMimetype,
+			fileStream: mockedFileStream,
+		});
+
+		const findUpdatedDrink = (await drinksRepositoryInMemory.findById(createdDrink._id.toString())) as Drink;
+
+		expect(findUpdatedDrink.cover).toBeDefined();
+		expect(findUpdatedDrink.cover?.mimetype).toEqual(fileMimetype);
+		expect(findUpdatedDrink.cover?.name).toContain(fileName);
+		expect(storageProvider.uploadFile).toHaveBeenCalledTimes(1);
+	});
+
+	it('should not be able to update the cover to a non-image file', async () => {
+		await expect(
+			service.execute({
+				drinkId: new ObjectId().toString(),
+				name: fileName,
+				mimetype: 'application/json',
+				fileStream: mockedFileStream,
+			}),
+		).rejects.toBeInstanceOf(BadRequestError);
+	});
+
+	it('should not be able to update the cover of a nonexistent drink', async () => {
+		await expect(
+			service.execute({
+				drinkId: new ObjectId().toString(),
+				name: fileName,
+				mimetype: fileMimetype,
+				fileStream: mockedFileStream,
+			}),
+		).rejects.toBeInstanceOf(BadRequestError);
+	});
+
+	it('should be able to delete the existing drink cover during the update', async () => {
+		await drinksRepositoryInMemory.update({
+			id: createdDrink._id.toString(),
+			cover: { name: fileMimetype, mimetype: fileMimetype, url: '' },
+		});
+
+		await service.execute({
+			drinkId: createdDrink._id.toString(),
+			name: fileName,
+			mimetype: fileMimetype,
+			fileStream: mockedFileStream,
+		});
+
+		expect(storageProvider.deleteFile).toHaveBeenCalledTimes(1);
+	});
+
+	it('should be able to add the file extension during the drink cover update', async () => {
+		await service.execute({
+			drinkId: createdDrink._id.toString(),
+			name: 'imageFileNameWithoutExtension',
+			mimetype: fileMimetype,
+			fileStream: mockedFileStream,
+		});
+
+		const foundDrink = (await drinksRepositoryInMemory.findById(createdDrink._id.toString())) as Drink;
+		const drinkCover = foundDrink.cover as FileMetadata;
+
+		expect(drinkCover).toBeDefined();
+		expect(drinkCover).toHaveProperty('name');
+		expect(hasFileExtension(drinkCover.name, fileMimetype)).toBe(true);
+	});
+});
