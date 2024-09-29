c0acca7955704f3415700f7b4843d5cd978ac41d
Juan Lessa <juanvlessa@ua.pt>
2024-09-29 10:25:51 +0100

    feat: :sparkles: delete drink attachments when delete a drink

--- src/core/drinks/useCases/drinks/delete/DeleteDrink.service.ts
+++ src/core/drinks/useCases/drinks/delete/DeleteDrink.service.ts
@@ -2,9 +2,13 @@ import { DRINK_MESSAGES } from '@/core/drinks/constants/drinks.constants';
 import { DeleteDrink } from '@/core/drinks/dtos/drink.dtos';
 import { IDrinksRepository } from '@/core/drinks/repositories/IDrinks.repository';
 import { BadRequestError } from '@/shared/error/error.lib';
+import { IStorageProvider } from '@/shared/providers/storage/IStorage.provider';
 
 export class DeleteDrinkService {
-	constructor(private drinksRepository: IDrinksRepository) {}
+	constructor(
+		private drinksRepository: IDrinksRepository,
+		private storageProvider: IStorageProvider,
+	) {}
 
 	async execute({ id }: DeleteDrink) {
 		const drinkExists = await this.drinksRepository.findById(id);
@@ -13,5 +17,12 @@ export class DeleteDrinkService {
 		}
 
 		await this.drinksRepository.delete(id);
+
+		if (drinkExists.cover) {
+			await this.storageProvider.deleteFile(drinkExists.cover.name);
+		}
+		if (drinkExists.thumbnail) {
+			await this.storageProvider.deleteFile(drinkExists.thumbnail.name);
+		}
 	}
 }
--- src/core/drinks/useCases/drinks/delete/deleteDrink.spec.ts
+++ src/core/drinks/useCases/drinks/delete/deleteDrink.spec.ts
@@ -2,7 +2,7 @@ import { Category } from '@/core/drinks/entities/category.entity';
 import { CategoriesRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Categories.repository';
 import { IngredientsRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Ingredients.repository';
 import { ObjectId } from 'mongodb';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { DeleteDrinkService } from './DeleteDrink.service';
 import { BadRequestError } from '@/shared/error/error.lib';
 import { createCategoryFactory, createDrinkFactory, createIngredientFactory } from '@/core/drinks/container';
@@ -11,10 +11,14 @@ import { IIngredientsRepository } from '@/core/drinks/repositories/IIngredients.
 import { IDrinksRepository } from '@/core/drinks/repositories/IDrinks.repository';
 import { DrinksRepositoryInMemory } from '@/core/drinks/repositories/inMemory/Drinks.repository';
 import { Ingredient } from '@/core/drinks/entities/ingredient.entity';
+import { IStorageProvider } from '@/shared/providers/storage/IStorage.provider';
+import { MockStorageProvider } from '@/shared/providers/storage/implementations/MockStorage.provider';
+import { Drink } from '@/core/drinks/entities/drink.entity';
 
 let categoriesRepositoryInMemory: ICategoriesRepository;
 let ingredientsRepositoryInMemory: IIngredientsRepository;
 let drinksRepositoryInMemory: IDrinksRepository;
+let storageProvider: IStorageProvider;
 let service: DeleteDrinkService;
 
 const { translations: categoryTranslations } = createCategoryFactory();
@@ -23,12 +27,17 @@ const { translations: ingredientTranslations, is_alcoholic: ingredientIsAlcoholi
 let createdIngredient: Ingredient;
 const { translations } = createDrinkFactory();
 
+let createdDrink: Drink;
+
 describe('Delete Drink', () => {
 	beforeEach(async () => {
+		vi.clearAllMocks();
+
 		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
 		ingredientsRepositoryInMemory = new IngredientsRepositoryInMemory();
 		drinksRepositoryInMemory = new DrinksRepositoryInMemory();
-		service = new DeleteDrinkService(drinksRepositoryInMemory);
+		storageProvider = new MockStorageProvider();
+		service = new DeleteDrinkService(drinksRepositoryInMemory, storageProvider);
 
 		createdCategory = await categoriesRepositoryInMemory.create({ translations: categoryTranslations });
 		createdIngredient = await ingredientsRepositoryInMemory.create({
@@ -36,13 +45,17 @@ describe('Delete Drink', () => {
 			is_alcoholic: ingredientIsAlcoholic,
 			category: createdCategory,
 		});
-	});
-	it('should be able to delete an drink', async () => {
-		const createdDrink = await drinksRepositoryInMemory.create({
+		createdDrink = await drinksRepositoryInMemory.create({
 			translations,
 			ingredients: [{ ingredient: createdIngredient, quantity: 60 }],
 		});
+	});
 
+	afterAll(async () => {
+		vi.clearAllMocks();
+	});
+
+	it('should be able to delete a drink', async () => {
 		await service.execute({ id: createdDrink._id.toString() });
 
 		const found = await drinksRepositoryInMemory.findById(createdDrink._id.toString());
@@ -53,5 +66,19 @@ describe('Delete Drink', () => {
 	it('should not be able to delete a nonexistent drink', async () => {
 		const nonexistentId = new ObjectId().toString();
 		await expect(service.execute({ id: nonexistentId })).rejects.toBeInstanceOf(BadRequestError);
+	});
+
+	it('should be able to delete the cover and thumbnail during delete a drink', async () => {
+		await drinksRepositoryInMemory.update({
+			id: createdDrink._id.toString(),
+			cover: { name: 'coverFile.png', mimetype: 'image/png', url: '' },
+			thumbnail: { name: 'thumbnailFile.jpeg', mimetype: 'image/jpeg', url: '' },
+		});
+
+		await service.execute({ id: createdDrink._id.toString() });
+
+		const found = await drinksRepositoryInMemory.findById(createdDrink._id.toString());
+
+		expect(storageProvider.deleteFile).toBeCalledTimes(2);
 	});
 });
