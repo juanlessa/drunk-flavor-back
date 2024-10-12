a47f71316eaf81c754bd895f1829feee2a1859dc
Juan Lessa <juanvlessa@ua.pt>
2024-10-13 00:00:50 +0100

    style: :art: Refactor parameter naming for clarity: prefix _

--- src/shared/providers/storage/implementations/MockStorage.provider.ts
+++ src/shared/providers/storage/implementations/MockStorage.provider.ts
@@ -3,6 +3,6 @@ import { IStorageProvider } from '../IStorage.provider';
 
 export class MockStorageProvider implements IStorageProvider {
 	getFileURL = vi.fn().mockImplementation((fileName: string) => `http://mock.test/${fileName}`);
-	deleteFile = vi.fn().mockImplementation((fileName: string) => {});
-	uploadFile = vi.fn().mockImplementation((fileName: string) => {});
+	deleteFile = vi.fn().mockImplementation((_fileName: string) => {});
+	uploadFile = vi.fn().mockImplementation((_fileName: string) => {});
 }
