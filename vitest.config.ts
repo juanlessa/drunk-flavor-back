378c8c19fe228866046d01ef098f33c85af49750
Juan Lessa <juanvlessa@ua.pt>
2024-10-19 02:21:25 +0100

    feat: :sparkles: configure coverage include and exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,16 @@ export default defineConfig({
 		include: ['src/**/*.spec.ts'],
 		exclude: ['src/**/*.e2e-spec.ts'],
 		environment: 'node',
+		coverage: {
+			include: ['src/core/**/*', 'src/shared/providers/**/*', 'src/shared/helpers/**/*'],
+			exclude: [
+				'src/core/**/infra/**/*',
+				'src/core/**/schemas/**/*',
+				'src/core/**/*.controller.ts',
+				'src/core/**/*.schema.ts',
+				'src/core/**/*.container.ts',
+				'src/shared/providers/**/index.ts',
+			],
+		},
 	},
 });
